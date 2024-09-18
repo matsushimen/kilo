7d1ee528befbfda229578d47ecf131727b5e3417
matsushimen <ukasustam@gmail.com>
2024-09-18 21:38:21 +0900

    Clear the screen on exit

--- kilo.c
+++ kilo.c
@@ -19,6 +19,9 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
   perror(s);
   exit(1);
 }
@@ -62,7 +65,9 @@ void editorRefreshScreen() {
 void editorProcessKeypress() {
   char c = editorReadKey();
   switch (c) {
-    case CTRL_KEY('q'):
+    case CTRL_KEY('Q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
   }
